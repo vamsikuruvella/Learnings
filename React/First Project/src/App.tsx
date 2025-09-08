13d48a75205d3d4a9bfdbc2d1e932897f7ebe4e4
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2025-09-08 14:51:15 +0000

    rr

--- React/First Project/src/App.tsx
+++ React/First Project/src/App.tsx
@@ -2,7 +2,7 @@ import Message from './message'
 import ListGroup from './components/ListGroup'
 
 function App(){
-  return <div><Message></Message></div>
+  return <div><ListGroup></ListGroup></div>
 }
 
 export default App;
